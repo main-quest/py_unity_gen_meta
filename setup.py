d51350fcf68f464132a559e6f5396bcfc2b2a037
Lucian Lazar <lucian@mainquest.ro>
2021-05-03 17:01:02 +0300

    doc update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='py_unity_gen_meta',
     version='0.0.6',
     description="Generates Unity .meta files in a specified directory. " +
-                "'Install by 'pip install git+https://github.com/tfgstudios/py_unity_gen_meta.git#egg=py_unity_gen_meta'",
+                "'Install by 'pip install --upgrade git+https://github.com/tfgstudios/py_unity_gen_meta.git#egg=py_unity_gen_meta'",
     url='https://github.com/tfgstudios/py_unity_gen_meta.git',
     author='The Fallen Games',
     author_email='contact@thefallengames.com',
